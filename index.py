1c62b6d7a9841b5ec6b28d7077450d26caf4c897
Ricardo Bizerra <ricardo.bizerra19@gmail.com>
2023-08-27 23:57:29 -0300

    feat(main): improving embed

--- index.py
+++ index.py
@@ -22,7 +22,7 @@ while branch != "":
     if branch != "": branches_to_monitor.append(branch)
 
 try:
-    since_time = (datetime.now() - timedelta(days=3)).isoformat()
+    since_time = (datetime.now() - timedelta(days=1)).isoformat()
     # since_time without timezone and microseconds and in ISO 8601 format
     since_time_ = since_time[0:22] + "Z"
 
@@ -41,25 +41,24 @@ try:
             commits = response.json()
             
             if commits:
+                description = ""
                 for commit in commits:
                     commit_sha = commit["sha"][0:7]
                     commit_message = commit["commit"]["message"]
-                    commit_author = commit["committer"]["login"]
+                    commit_author = commit["author"]["login"]
+                    commit_author_url = commit["author"]["html_url"]
+                    commit_icon_url = commit["author"]["avatar_url"]
                     commit_url = commit["html_url"]
+                    description += f"[`{commit_sha}`]({commit_url}) {commit_message}\n"
 
-                    # Send notification to Discord
-                    webhook = DiscordWebhook(url=discord_webhook_url)
-                    embed = DiscordEmbed(title="New Commit Detected",
-                                            description=commit_message,
-                                            color="3D7C4E")
-                    embed.set_author(name=commit_author, icon_url=commit["author"]["avatar_url"])
-                    embed.add_embed_field(name="Author", value=commit_author, inline=False)
-                    embed.add_embed_field(name="Commit URL", value=commit_url, inline=False)
-                    embed.add_embed_field(name="Branch", value=branch, inline=False)
-                    webhook.add_embed(embed)
-                    webhook.execute()
-
-                    print(f"New commit detected: {commit_sha} - {commit_message}")
+                # Send notification to Discord
+                webhook = DiscordWebhook(url=discord_webhook_url)
+                embed = DiscordEmbed(title=f"[{github_repo.split('/')[1]}:{branch}] {len(commits)} new commits",
+                                        description=description,
+                                        color="3D7C4E")
+                embed.set_author(name=commit_author, url=commit_author_url, icon_url=commit_icon_url)
+                webhook.add_embed(embed)
+                webhook.execute()
 
 except Exception as e:
     print(f"An error occurred: {e}")
