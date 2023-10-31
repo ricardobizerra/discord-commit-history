302578b3eb9a632e9f32bba4235755433a39f8f6
ricardobizerra <ricardo.bizerra19@gmail.com>
2023-10-31 11:39:47 -0300

    feat: user can define since & until days to monitor

--- index.py
+++ index.py
@@ -20,17 +20,23 @@ branches_to_monitor = []
 while branch != "":
     branch = input("Enter the branches you want to monitor: ")
     if branch != "": branches_to_monitor.append(branch)
+since_days = int(input("Enter the number of days to monitor: "))
+until_days = int(input("Enter the number of days to monitor: ") or 0)
 
 try:
-    since_time = (datetime.now() - timedelta(days=1)).isoformat()
+    since_time = (datetime.now() - timedelta(days=since_days)).isoformat()
     # since_time without timezone and microseconds and in ISO 8601 format
     since_time_ = since_time[0:22] + "Z"
+
+    until_time = (datetime.now() - timedelta(days=until_days)).isoformat()
+    # until_time without timezone and microseconds and in ISO 8601 format
+    until_time_ = until_time[0:22] + "Z"
 
     for github_repo in repos_to_monitor:
 
         for branch in branches_to_monitor:
             response = requests.get(f"https://api.github.com/repos/{github_repo}/commits",
-                                    params={"sha": branch, "since": since_time},
+                                    params={"sha": branch, "since": since_time, "until": until_time},
                                     headers={
                                         "Authorization": f"Bearer {github_token}",
                                         "X-GitHub-Api-Version": "2022-11-28",
@@ -52,8 +58,12 @@ try:
                     description += f"[`{commit_sha}`]({commit_url}) {commit_message}\n"
 
                 # Send notification to Discord
+                if len(commits) == 1:
+                    title = f"[{github_repo.split('/')[1]}:{branch}] {len(commits)} new commit"
+                else:
+                    title = f"[{github_repo.split('/')[1]}:{branch}] {len(commits)} new commits"
                 webhook = DiscordWebhook(url=discord_webhook_url)
-                embed = DiscordEmbed(title=f"[{github_repo.split('/')[1]}:{branch}] {len(commits)} new commits",
+                embed = DiscordEmbed(title=title,
                                         description=description,
                                         color="3D7C4E")
                 embed.set_author(name=commit_author, url=commit_author_url, icon_url=commit_icon_url)
